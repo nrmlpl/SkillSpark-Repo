bcf299dab390682c3d3e6256611051a699af1a2b
Nirmal Pal <142670318+nrmlpl@users.noreply.github.com>
2025-02-15 09:02:05 +0530

    setting up for hosting

---
+++ server/utils/mailSender.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title,body) => {
+    try{
+        let transporter = nodemailer.createTransport({
+            host : process.env.MAIL_HOST,
+            auth:{
+                pass : process.env.MAIL_PASS,
+                user : process.env.MAIL_USER,
+            }
+
+        })
+
+        let info = await transporter.sendMail({
+            from: 'SkillSpark ||  by NIRMAL',
+            to:`${email}`,
+            subject: `Mail by SkiilSpark Organization`,
+            html: `${body}`,
+        });
+        console.log(info);
+        return info;
+
+    } catch(error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = mailSender;
